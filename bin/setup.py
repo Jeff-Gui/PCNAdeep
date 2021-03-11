8cfa740f7cc63a0a19ca74c8331a431c6ee2a774
Jeff-Gui <1964728070@qq.com>
2021-03-11 12:18:34 +0800

    Create setup.py

---
+++ bin/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pcnaDeep',
+      version='0.1',
+      description='deep learning pipeline for PCNA-based cell cycle profiling',
+      url='https://github.com/Jeff-Gui/PCNAdeep',
+      author='Yifan Gui',
+      author_email='Yifan.18@intl.zju.edu.cn',
+      license='Apache-2.0',
+      packages=['pcnaDeep', 'pcnaDeep.data'],
+      zip_safe=False)
