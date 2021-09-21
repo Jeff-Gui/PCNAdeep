5a7fae360db020206c76bb7d7dfb41a3f0fd1613
Jeff-Gui <1964728070@qq.com>
2021-09-21 15:22:29 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../bin/pcnaDeep'))
-
+sys.path.insert(0, os.path.abspath('../bin/'))
+import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
