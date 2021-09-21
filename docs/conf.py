7cc7d55520c181010507bcaf7be818259bfb9373
Jeff-Gui <1964728070@qq.com>
2021-09-21 15:56:55 +0800

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../bin/'))
 import sphinx_rtd_theme
+sys.path.insert(0, os.path.abspath('../bin'))
+print(sys.path)
 
 # -- Project information -----------------------------------------------------
 
